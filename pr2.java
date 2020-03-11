9162da6c55355dc2561b075eb98330892b1bb5c3
MinJae <guswlfahs123@naver.com>
2020-03-11 20:18:52 +0900

    pr2.java 두 정숫값 사이 숫자 출력

---
+++ pr2.java
@@ -0,0 +1,73 @@
+[200~^
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+pr2.java [unix] (08:59 01/01/1970)                                      0,1 모두
+-- 끼워넣기 --                                                       ^V
+^
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+pr2.java [unix] (08:59 01/01/1970)                                      0,1 모두
+-- 끼워넣기 --                                                       ^V
+package ex1.io;
+import java.util.Scanner;
+public class Program {
+
+	public static void main(String[] args) {
+	
+		Scanner scan = new Scanner(System.in);
+		System.out.print(" 정수 A 입력 : ");int num1=scan.nextInt();
+		System.out.print(" 정수 B 입력 : ");int num2=scan.nextInt();
+		int change; //만약 A값이 크다면 바꿔줄 변수 
+		int print=num1;
+		do {
+			if(num1>num2)
+			{
+				print=num2;
+				change=num1;
+				num1=num2;
+				num2=change;
+			}
+			System.out.print(print+" ");
+			print++;
+		} while(print<=num2);
+	}
+}
+

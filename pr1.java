175a393578678acf32f6c338fad8338a6e2337aa
kmj980521 <61898890+kmj980521@users.noreply.github.com>
2020-03-11 20:42:21 +0900

    Update pr1.java

--- pr1.java
+++ pr1.java
@@ -5,25 +5,25 @@ public class Program {
 
 		public static void main(String[] args) {
 				
-					Random rand = new Random();
-							Scanner scan = new Scanner(System.in);
-									int answer = (rand.nextInt(90)+10);
-											int num;
-													do {
-																	System.out.print("두 자리 정수 입력 : ");
-																				num = scan.nextInt();
+			Random rand = new Random();
+			Scanner scan = new Scanner(System.in);
+			int answer = (rand.nextInt(90)+10);
+			int num;
+		do {
+			System.out.print("두 자리 정수 입력 : ");
+			num = scan.nextInt();
 																							
-																							if(num>answer)
-																											{
-																																System.out.println("정답 보다 값이 큽니다..");
-																																			}
-																										else if (num<answer)
-																														{
-																																			System.out.println("정답 보다 값이 작습니다..");
-																																						}
-																													else
-																																		System.out.println("정답입니다..");
-																															} while(num!=answer);
-														}
+			if(num>answer)
+			{
+			`	System.out.println("정답 보다 값이 큽니다..");
+			}
+			else if (num<answer)
+			{
+				System.out.println("정답 보다 값이 작습니다..");
+			}
+			else
+				System.out.println("정답입니다..");
+		} while(num!=answer);
+	}
 }
 
